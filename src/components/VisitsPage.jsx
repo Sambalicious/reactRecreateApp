e3df76463432269f2673ff629addc1d89c5d88f7
Sambalicious <stemitope370@gmail.com>
2020-03-22 11:31:17 +0100

    second component updated

--- src/components/VisitsPage.jsx
+++ src/components/VisitsPage.jsx
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const VisitsPage = () => {
     return (  
-        <div>
-            hello from Visits
+        <Fragment>
+             <h2> Most Visited Places </h2>
+            <div className="grid-container">
+                <div className="card">
+                    <div className="bg-img box-1"> </div>
+                    <div className="content">
+                        <h4>Title</h4>
+                        <p>hello world! this is some text to show what will be rendered</p>
+                        <button>Read More</button>
+                    </div>
+                    </div>
+
+
+                    <div className="card">
+                    <div className="bg-img box-2"> </div>
+                    <div className="content">
+                        <h4>Whispering Palm, Lagos</h4>
+                        <p>hello world! this is some text to show what will be rendered</p>
+                        <button>Read More</button>
+                    </div>
+                    </div>
+
+                    <div className="card">
+                    <div className="bg-img box-3"> </div>
+                    <div className="content">
+                        <h4>Title</h4>
+                        <p>hello world! this is some text to show what will be rendered</p>
+                        <button>Read More</button>
+                    </div>
+                    </div>
+
+                    <div className="card">
+                    <div className="bg-img box-4"> </div>
+                    <div className="content">
+                        <h4>Title</h4>
+                        <p>hello world! this is some text to show what will be rendered</p>
+                        <button>Read More</button>
+                    </div>
+                    </div>
+
+                    <div className="card">
+                    <div className="bg-img box-5"> </div>
+                    <div className="content">
+                        <h4>Title</h4>
+                        <p>hello world! this is some text to show what will be rendered</p>
+                        <button>Read More</button>
+                    </div>
+                    </div>
+
+                    <div className="card">
+                    <div className="bg-img box-6"> </div>
+                    <div className="content">
+                        <h4>Title</h4>
+                        <p>hello world! this is some text to show what will be rendered</p>
+                        <button>Read More</button>
+                    </div>
+                    </div>
+
+
+
         </div>
+
+        
+            
+        </Fragment>
+           
+        
     );
 }
  
