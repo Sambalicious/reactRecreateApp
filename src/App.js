024005c5e2b4f6bbc01a50cba5c3161803ede4f3
Sambalicious <stemitope370@gmail.com>
2020-03-29 23:47:55 +0100

    page loading up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import LandingPage from './components/landingPage/LandingPage';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import FindPage from './components/Findpage/FindPage';
 
 
 
 const App = () => {
     return ( 
         <div>
-            <LandingPage />
+            <Router>
+                <Switch>
+                <Route path="/" exact component={LandingPage } />
+                <Route path='/find' exact component={FindPage} />
+                </Switch>
+            </Router>
+          
         </div>
      );
 }
---
+++ src/components/Findpage/FindPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../Navbar/Navbar'
+import Find from './Find'
+
+
+const FindPage = () => {
+    return ( 
+        <div>
+            <NavBar />
+            <Find />
+        </div>
+     );
+}
+ 
+export default FindPage;
