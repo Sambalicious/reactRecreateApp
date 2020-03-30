20d482561f024fb1f9812d544246ba4b29a2a5b2
Sambalicious <stemitope370@gmail.com>
2020-03-30 01:35:58 +0100

    addded finishing touches to the special offer page

--- src/components/SpecialOffer/SpecialOffer.jsx
+++ src/components/SpecialOffer/SpecialOffer.jsx
@@ -12,6 +12,9 @@ const SpecialOffer = () => {
             <h2 className="text-center m-6 text-5xl font-bold border-t border-black ">Special Offers</h2>
             <Special pics={zuma} title={'Zuma Rock'} />
             <Special pics={omu} title={'Omu Resort'} />
+            <div className="flex justify-center">
+                <button >See More</button>
+            </div>
         </div>
      );
 }
